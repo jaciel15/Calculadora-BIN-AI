cfa05b80335e17798d0592358bec9ee41a2cef52
jaciel <jacielf86@gmail.com>
2026-06-16 13:08:20 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,94 @@
+const openBtn =
+document.getElementById("openBtn");
+
+const fileInput =
+document.getElementById("fileInput");
+
+openBtn.onclick = () =>
+fileInput.click();
+
+fileInput.addEventListener(
+"change",
+loadBIN
+);
+
+async function loadBIN(event){
+
+    const file =
+    event.target.files[0];
+
+    if(!file) return;
+
+    const buffer =
+    await file.arrayBuffer();
+
+    const bytes =
+    new Uint8Array(buffer);
+
+    currentBIN =
+    createBINObject(
+        file,
+        bytes
+    );
+
+    document.getElementById(
+        "fileName"
+    ).textContent =
+    currentBIN.fileName;
+
+    document.getElementById(
+        "fileSize"
+    ).textContent =
+    currentBIN.fileSize;
+
+    showHEX(bytes);
+
+}
+
+function showHEX(bytes){
+
+    let output = "";
+
+    for(
+        let i=0;
+        i<bytes.length;
+        i+=16
+    ){
+
+        let offset =
+        i.toString(16)
+        .toUpperCase()
+        .padStart(4,"0");
+
+        output += offset + " : ";
+
+        for(
+            let j=0;
+            j<16;
+            j++
+        ){
+
+            if(
+                i+j < bytes.length
+            ){
+
+                output +=
+                bytes[i+j]
+                .toString(16)
+                .toUpperCase()
+                .padStart(2,"0")
+                + " ";
+
+            }
+
+        }
+
+        output += "\n";
+
+    }
+
+    document.getElementById(
+        "hexViewer"
+    ).textContent = output;
+
+}
